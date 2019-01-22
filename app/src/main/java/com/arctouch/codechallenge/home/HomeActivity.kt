c018c4b3a98c0739c7c96434c8091034817863aa
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-01-21 23:56:48 -0300

    improved home screen code

--- app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
+++ app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
@@ -1,32 +1,82 @@
 package com.arctouch.codechallenge.home
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import com.arctouch.codechallenge.R
-import com.arctouch.codechallenge.base.BaseActivity
-import com.arctouch.codechallenge.data.Cache
-import com.arctouch.codechallenge.util.API_KEY
-import com.arctouch.codechallenge.util.DEFAULT_LANGUAGE
-import com.arctouch.codechallenge.util.DEFAULT_REGION
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
+import com.arctouch.codechallenge.model.Movie
+import com.arctouch.codechallenge.util.Status
+import com.arctouch.codechallenge.viewmodel.UpcomingMoviesViewModel
 import kotlinx.android.synthetic.main.home_activity.*
 
-class HomeActivity : BaseActivity() {
+class HomeActivity : AppCompatActivity() {
+
+    private var homeAdapter: HomeAdapter? = null
+    private val moviesList: MutableList<Movie> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
 
-        api.upcomingMovies(API_KEY, DEFAULT_LANGUAGE, 1, DEFAULT_REGION)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe {
-                val moviesWithGenres = it.results.map { movie ->
-                    movie.copy(genres = Cache.genres.filter { movie.genreIds?.contains(it.id) == true })
+        setupRecyclerView()
+        loadContent()
+
+        buttonRetry.setOnClickListener {
+            loadContent()
+        }
+
+//        api.upcomingMovies(API_KEY, DEFAULT_LANGUAGE, 1, DEFAULT_REGION)
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe {
+//                val moviesWithGenres = it.results.map { movie ->
+//                    movie.copy(genres = Cache.genres.filter { movie.genreIds?.contains(it.id) == true })
+//                }
+//                recyclerView.adapter = HomeAdapter(moviesWithGenres)
+//                progressBar.visibility = View.GONE
+//            }
+    }
+
+    private fun loadContent() {
+        val viewModel = ViewModelProviders.of(this).get(UpcomingMoviesViewModel::class.java)
+        viewModel.getUpcomingMovies()?.observe(this, Observer { resource ->
+            when(resource?.status) {
+                Status.LOADING -> {
+                    setVisibility(View.VISIBLE, View.GONE, View.GONE, View.GONE)
                 }
-                recyclerView.adapter = HomeAdapter(moviesWithGenres)
-                progressBar.visibility = View.GONE
+                Status.ERROR -> {
+                    errorMessage.text = resource.message
+
+                    setVisibility(View.GONE, View.GONE, View.GONE, View.VISIBLE)
+                }
+                Status.SUCCESS -> {
+                    resource.data?.let { moviesListNonNull ->
+                        moviesList.addAll(moviesListNonNull.results)
+                        homeAdapter?.notifyDataSetChanged()
+
+                        setVisibility(View.GONE, View.VISIBLE, View.GONE, View.GONE)
+                    } ?: run {
+                        setVisibility(View.GONE, View.GONE, View.VISIBLE, View.GONE)
+                    }
+                }
             }
+        })
+    }
+
+    private fun setVisibility(progress: Int, recycler: Int, noContent: Int, error: Int) {
+        progressBar.visibility = progress
+        contentLayout.visibility = recycler
+        no_content.visibility = noContent
+        errorLayout.visibility = error
+    }
+
+    private fun setupRecyclerView() {
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        homeAdapter = HomeAdapter(moviesList)
+        recyclerView.adapter = homeAdapter
     }
 }
--- app/src/main/java/com/arctouch/codechallenge/splash/SplashActivity.kt
+++ app/src/main/java/com/arctouch/codechallenge/splash/SplashActivity.kt
@@ -2,28 +2,25 @@ package com.arctouch.codechallenge.splash
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.arctouch.codechallenge.R
-import com.arctouch.codechallenge.base.BaseActivity
-import com.arctouch.codechallenge.data.Cache
 import com.arctouch.codechallenge.home.HomeActivity
-import com.arctouch.codechallenge.util.API_KEY
-import com.arctouch.codechallenge.util.DEFAULT_LANGUAGE
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 
-class SplashActivity : BaseActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_activity)
 
-        api.genres(API_KEY, DEFAULT_LANGUAGE)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe {
-                Cache.cacheGenres(it.genres)
-                startActivity(Intent(this, HomeActivity::class.java))
-                finish()
-            }
+        startActivity(Intent(this, HomeActivity::class.java))
+
+//        api.genres(API_KEY, DEFAULT_LANGUAGE)
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe {
+//                Cache.cacheGenres(it.genres)
+//                startActivity(Intent(this, HomeActivity::class.java))
+//                finish()
+//            }
     }
 }
