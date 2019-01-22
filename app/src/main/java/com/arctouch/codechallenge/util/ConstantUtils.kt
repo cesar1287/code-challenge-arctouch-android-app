62c9e0dab3bba7f38044348eac6e1803d13c0e6b
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2019-01-22 11:30:56 -0300

    remove language and region to test pagination

--- app/src/main/java/com/arctouch/codechallenge/util/ConstantUtils.kt
+++ app/src/main/java/com/arctouch/codechallenge/util/ConstantUtils.kt
@@ -2,8 +2,9 @@ package com.arctouch.codechallenge.util
 
 const val URL = "https://api.themoviedb.org/3/"
 const val API_KEY = "1f54bd990f1cdfb230adb312546d765d"
-const val DEFAULT_LANGUAGE = "pt-BR"
-const val DEFAULT_REGION = "BR"
+const val DEFAULT_LANGUAGE = ""
+const val DEFAULT_REGION = ""
 const val POSTER_URL = "https://image.tmdb.org/t/p/w154"
 const val BACKDROP_URL = "https://image.tmdb.org/t/p/w780"
 const val ERROR_DEFAULT = "unknown error"
+const val PAGE_SIZE = 20
